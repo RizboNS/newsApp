f803ad08fed96b6e3da5dde9da1acbb1840bc7ab
rizbons <rizbo.ns.bk@gmail.com>
2022-11-25 13:53:28 +0100

    Init create-news page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNewsComponent } from './pages/create-news/create-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin/create-news', component: CreateNewsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/create-news/create-news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-news',
+  templateUrl: './create-news.component.html',
+  styleUrls: ['./create-news.component.css']
+})
+export class CreateNewsComponent {
+
+}
