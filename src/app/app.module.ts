bcd4e88dbce9cc388fdb8bb4979e4cc095950c9e
rizbons <rizbo.ns.bk@gmail.com>
2022-11-26 21:27:31 +0100

    init quill js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateNewsComponent } from './pages/create-news/create-news.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateNewsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CreateNewsComponent],
+  imports: [BrowserModule, AppRoutingModule, QuillModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/create-news/create-news.component.ts
+++ src/app/pages/create-news/create-news.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-news',
   templateUrl: './create-news.component.html',
-  styleUrls: ['./create-news.component.css']
+  styleUrls: ['./create-news.component.css'],
 })
-export class CreateNewsComponent {
-
+export class CreateNewsComponent implements OnInit {
+  editorContent!: string;
+  editorStyle = {
+    height: '80vh',
+  };
+  editorForm!: FormGroup;
+  ngOnInit(): void {
+    this.editorForm = new FormGroup({
+      editor: new FormControl(null),
+    });
+  }
+  onSubmit(): void {
+    this.editorContent = this.editorForm.get('editor')?.value;
+    console.log(this.editorForm.get('editor')?.value);
+  }
 }
