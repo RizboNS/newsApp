a3630ec7b4aff66ed4b962f3b42fe78423c3d3cc
rizbons <rizbo.ns.bk@gmail.com>
2023-01-02 05:12:43 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,12 @@ import { QuillModule } from 'ngx-quill';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateNewsComponent } from './pages/create-news/create-news.component';
-import { UpdateNewsComponent } from './pages/update-news/update-news.component';
 import { StoryComponent } from './components/story/story.component';
 import { SharedModule } from './shared/shared.module';
 import { AdminModule } from './admin/admin.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateNewsComponent,
-    UpdateNewsComponent,
-    StoryComponent,
-  ],
+  declarations: [AppComponent, StoryComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
