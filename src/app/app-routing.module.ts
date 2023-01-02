693bcbbaffc5cf21c2dc1da07278b123ad09ea7c
rizbons <rizbo.ns.bk@gmail.com>
2023-01-02 05:14:54 +0100

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateNewsComponent } from './pages/create-news/create-news.component';
 
 const routes: Routes = [
-  { path: 'test', component: CreateNewsComponent },
   {
     path: 'admin',
     loadChildren: () =>
