0dc874c5f5d5982767ff74d433ce34cc48774132
rizbons <rizbo.ns.bk@gmail.com>
2023-01-05 05:12:07 +0100

    tweaking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoryComponent } from './components/story/story.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  { path: 'story', component: StoryComponent }, // TO DO - add route params and change to serbian
   { path: '', component: HomeComponent },
   {
     path: 'admin',
