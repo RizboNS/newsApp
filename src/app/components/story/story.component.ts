3484738584c095401e5d7af094561f8e90d7c124
rizbons <rizbo.ns.bk@gmail.com>
2023-01-10 06:30:27 +0100

    fetch from Api success

--- src/app/components/story/story.component.ts
+++ src/app/components/story/story.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-story',
@@ -8,13 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class StoryComponent implements OnInit {
   titleId: string = '';
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private newsService: NewsService
+  ) {}
   ngOnInit(): void {
     this.asignTitleId();
+    this.getStory();
   }
   asignTitleId(): void {
     this.route.params.subscribe((params) => {
       this.titleId = params['titleId'];
     });
+  }
+  getStory(): void {
+    this.newsService
+      .getStoryByTitleId(this.titleId)
+      .pipe(take(1))
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
