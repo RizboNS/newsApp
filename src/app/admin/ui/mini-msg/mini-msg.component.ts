d462efdcfbe8a3e76995bbe911b0962ac362cf6d
rizbons <rizbo.ns.bk@gmail.com>
2023-01-16 04:51:04 +0100

    onSuccess() and styling

--- src/app/admin/ui/mini-msg/mini-msg.component.ts
+++ src/app/admin/ui/mini-msg/mini-msg.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-mini-msg',
   templateUrl: './mini-msg.component.html',
-  styleUrls: ['./mini-msg.component.css']
+  styleUrls: ['./mini-msg.component.css'],
 })
 export class MiniMsgComponent {
+  @ViewChild('savedMsgEl') savedMsgEl!: ElementRef;
+  @ViewChild('savedMsgContainer') savedMsgContainer!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  onSuccessMsg(): void {
+    const newSavedMsgEl = this.renderer.createElement('p');
+    const content = this.renderer.createText('Success...');
+    this.renderer.appendChild(newSavedMsgEl, content);
+    this.renderer.addClass(newSavedMsgEl, 'savedMsg');
+    this.renderer.addClass(newSavedMsgEl, 'savedMsgShow');
+    this.renderer.appendChild(
+      this.savedMsgContainer.nativeElement,
+      newSavedMsgEl
+    );
+  }
 }
