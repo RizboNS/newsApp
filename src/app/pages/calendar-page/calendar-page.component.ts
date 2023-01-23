5f87704950344bcf4918b690ed84c0c600c11abd
rizbons <rizbo.ns.bk@gmail.com>
2023-01-23 16:02:08 +0100

    animation

--- src/app/pages/calendar-page/calendar-page.component.ts
+++ src/app/pages/calendar-page/calendar-page.component.ts
@@ -1,7 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { calendarDummyData } from 'src/app/models/calendar-events-by-day.model';
-
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 @Component({
+  animations: [
+    trigger('eventAnimations', [
+      state(
+        'open',
+        style({
+          height: '*',
+          opacity: 1,
+          transform: 'translateY(0)',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'translateY(-50%)',
+        })
+      ),
+      transition('open => closed', [animate('0.3s')]),
+      transition('closed => open', [animate('0.3s')]),
+    ]),
+  ],
   selector: 'app-calendar-page',
   templateUrl: './calendar-page.component.html',
   styleUrls: ['./calendar-page.component.css'],
@@ -35,12 +63,9 @@ export class CalendarPageComponent implements OnInit {
     });
   }
   flip(index: number) {
-    // if (e.target.classList.contains('calendar-event-header')) {
-    //   e.target.nextElementSibling.classList.toggle('show');
-    //   e.target.lastChild.classList.toggle('show');
-    // } else {
-    //   e.target.parentElement.nextElementSibling.classList.toggle('show');
-    // }
     this.selectedEventIndex = index === this.selectedEventIndex ? -1 : index;
+  }
+  getEventState(index: number) {
+    return index === this.selectedEventIndex ? 'open' : 'closed';
   }
 }
