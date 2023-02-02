5b4650da36bf9a33403355adf670ba8d84ee037d
rizbons <rizbo.ns.bk@gmail.com>
2023-02-02 08:01:43 +0100

    drop-down animation and logic on close and open

--- src/app/shared/ui/drop-down/drop-down.component.ts
+++ src/app/shared/ui/drop-down/drop-down.component.ts
@@ -1,37 +1,79 @@
-import { animate, style, transition, trigger } from '@angular/animations';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-drop-down',
   templateUrl: './drop-down.component.html',
   styleUrls: ['./drop-down.component.css'],
   animations: [
-    trigger('dropDown', [
-      transition(':enter', [
-        style({ height: '0', opacity: 0 }),
-        animate('0.2s ease-out', style({ height: '*', opacity: 1 })),
-      ]),
-      transition(':leave', [
-        style({ height: '*', opacity: 1 }),
-        animate('0.2s ease-out', style({ height: '0', opacity: 0 })),
-      ]),
+    trigger('eventAnimationsState', [
+      state(
+        'open',
+        style({
+          height: '*',
+          opacity: 1,
+          transform: 'translateY(0)',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '0',
+          opacity: 0,
+          transform: 'translateY(-50%)',
+        })
+      ),
+      transition('open => closed', [animate('0.3s')]),
+      transition('closed => open', [animate('0.3s')]),
     ]),
   ],
 })
 export class DropDownComponent {
+  @ViewChild('dropDownMenu', { static: true }) dropDownMenu!: ElementRef;
+  @ViewChild('dropDownLabel', { static: true }) dropDownLabel!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (
+      !this.dropDownMenu.nativeElement.contains(event.target) &&
+      !this.dropDownLabel.nativeElement.contains(event.target)
+    ) {
+      this.showOptions = false;
+    }
+  }
+
   @Input() label: string = '';
   @Input() options: string[] = [];
   @Input() selectedOptionIndex: number = 0;
   @Output() onOptionSelectedIndexChange: EventEmitter<number> =
     new EventEmitter<number>();
+
   showOptions = false;
+
   constructor() {}
+
+  getEventState() {
+    return this.showOptions ? 'open' : 'closed';
+  }
 
   onOptionSelected(option: number) {
     this.onOptionSelectedIndexChange.emit(option);
   }
   showOptionsToggle() {
     this.showOptions = !this.showOptions;
-    console.log(this.showOptions);
   }
 }
